859bb7aad859f6dbb0bf19ca92beeca54b99e9b9
Faroukii <122316189+Faroukii@users.noreply.github.com>
2023-02-12 20:38:26 -0500

    party

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please enter your party affiliation ");
+        String partyAffiliation = "independent";
+        String independent = "man";
+        String democrat = "donkey";
+        String republican = "elephant";
+        String other = "other";
+
+        if (partyAffiliation == "democrat") {
+            System.out.println("You have received a: " + democrat);
+        } else {
+            (System.out.println("You have received a: " + independent);
+        }
+        {(System.out.println("You have received a: " + republican);}
+
+
+    {
+            System.out.println("You have received: " + other);
+        }
+
+
+
+
+
+    }
+}
